9e0086f7630212a81bc54cca7ebee1081907eb61
Vasika-uso <iovunicu@mail.ru>
2022-05-07 11:47:44 +0300

    real time chart

--- Memory_Manager/SelfBarSet.h
+++ Memory_Manager/SelfBarSet.h
@@ -7,11 +7,11 @@
 class SelfBarSet{
 public:
     std::string dataForDiskBar = " ";
-    std::map<int , float> BarSetData;
+    std::map<int , std::string> BarSetData;
     QBarSet* set0 = new QBarSet("");
     SelfBarSet();
-    void add_data(std::vector<float> data);
-    float get_value(int index);
+    void add_data(std::vector<std::string> _data , std::vector<float> data);
+    std::string get_value(int index);
 
 
 
@@ -19,18 +19,20 @@ public:
 SelfBarSet::SelfBarSet(){
 
 }
-float SelfBarSet::get_value(int index){
+std::string SelfBarSet::get_value(int index){
     for (const auto& [__index, value] : BarSetData) {
         if(index == __index){
             return value;
         }
     }
 }
-void SelfBarSet::add_data(std::vector<float> data){
-    for(int i = 0; i < data.size(); i++){
+void SelfBarSet::add_data(std::vector<std::string> _data , std::vector<float> data){
+    BarSetData.clear();
+    for(int i = 0; i < (int)data.size(); i++){
         //here insted of first data[i] is gonna be the folder/file path    //1
-        BarSetData.insert(std::make_pair(i , data[i] /* 1 */));
+        BarSetData.insert(std::make_pair(i , _data[i] /* 1 */));
         // instead of the second is gonna be the bytes/kb value && get_value is going to get the path and the bytes shown in the list;
+
         set0->insert(i , data[i]);
     }
 }
--- Memory_Manager/main.cpp
+++ Memory_Manager/main.cpp
@@ -8,7 +8,7 @@
 
 
 
-bool terminateThread1 = false;
+
 std::map<std::string , long> /*std::map<std::string , double>*/ ListItems(std::string dir){
     std::map<std::string , long> __list;
     QDir source(QString::fromStdString((dir)));
@@ -26,18 +26,31 @@ std::map<std::string , long> /*std::map<std::string , double>*/ ListItems(std::s
 
 }
 
-void updateList(QTextEdit* upperBar , QListWidget* list){
+void updateList(QTextEdit* upperBar , QListWidget* list , QChartView* w , QChart *chart){
+
     list->clear();
     QString target_dir = upperBar->toPlainText();
-    std::cout << target_dir.toStdString() << std::endl;
+    //std::cout << target_dir.toStdString() << std::endl;
     std::map<std::string , long> list_Data = ListItems(target_dir.toStdString());
+    if(list_Data.size() == 0){
+        return;
+    }
     std::vector<float> chart_Data;
+    std::vector<std::string> chart_Names;
     for (auto const& [dir, size] : list_Data)
     {
         chart_Data.push_back((float)size);
         std::string itemData = dir;
+        chart_Names.push_back(itemData);
         list->addItem(QString(itemData.c_str()));
     }
+    SelfBarSet *set1 = new SelfBarSet();
+    QBarSeries *series1 = new QBarSeries();
+    set1->add_data(chart_Names , chart_Data);
+    series1->append(set1->set0);
+    chart->removeAllSeries();
+    w->setChart(chart);
+    chart->addSeries(series1);
 }
 
 
@@ -91,16 +104,21 @@ int main(int argc, char *argv[])
 
 
         QString target_dir = upperBar->toPlainText();
+        if(target_dir.toStdString() == ""){
+            target_dir = ".";
+        }
         std::map<std::string , long> list_Data = ListItems(target_dir.toStdString());
         std::vector<float> chart_Data;
+        std::vector<std::string> chart_Names;
         for (auto const& [dir, size] : list_Data)
         {
             chart_Data.push_back((float)size);
-            std::string itemData = dir + std::string("     bytes: ") + std::to_string(size);
+            std::string itemData = dir;
+            chart_Names.push_back(itemData);
             list->addItem(QString(itemData.c_str()));
         }
 
-        set->add_data(chart_Data);
+        set->add_data(chart_Names , chart_Data);
         series->append(set->set0);
         w.setChart(chart);
         chart->addSeries(series);
@@ -116,7 +134,7 @@ int main(int argc, char *argv[])
 
         QObject::connect(set->set0, &QBarSet::hovered, [&diskBar, &set , &w, &hoverItem](bool status, int index){
 
-            diskBar->setText(QString::fromStdString(std::string("clicked index nr --> ") + std::to_string((int)set->get_value(index))));
+            diskBar->setText(QString::fromStdString(std::string("clicked index nr --> ") + set->get_value(index)));
             QPoint p = w.mapFromGlobal(QCursor::pos());
             if(status){
                 QGraphicsItem *it = w.itemAt(p);
@@ -129,8 +147,10 @@ int main(int argc, char *argv[])
                 hoverItem.hide();
             }
         });
-        QObject::connect(upperBar, &QTextEdit::textChanged, [&upperBar , &list](){
-            std::thread update_list(&updateList , upperBar , list);
+        QObject::connect(upperBar, &QTextEdit::textChanged, [&upperBar , &list , &w , &chart](){
+            //updateList(QTextEdit* upperBar , QListWidget* list , QChartView* w , SelfBarSet *set , QBarSeries *series , QChart *chart)
+            chart->removeAllSeries();
+            std::thread update_list(&updateList , upperBar, list, &w, chart);
             update_list.detach();
         });
 
